f6c5a64ff7b3c9dd100cfb0cc99749cd91aa81df
Poze <110929031+ArturZEN1@users.noreply.github.com>
2024-06-11 16:20:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,41 @@ const textoResultado = document.querySelector('.texto.resultado');
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1" ,
-        alternativas: [ "Alternativas 1",
-        "Alternativa 2"
+        enunciado: "Quais são as principais comidas de Maringá?" ,
+        alternativas: [ "Cachorros quentes ",
+        "Burrito"
 
         ]
-    }
+    },
+{
+       enunciado: "Quais são os principais pontos turisticos de Maringá?" ,
+    alternativas: [ "Cristo redentor, torre elfil, obelisco.",
+    "Obelisco, Catedral, praça do peladão "
+
+    ]
+},
+
+{
+     enunciado: "Qual é a história da catedral? " ,
+    alternativas: [ "",
+    "Alternativa 2"
+
+]
+},
+
+{
+    enunciado: "qual os principal evento de Maringá?" ,
+    alternativas: [ "expoingá",
+    ""
+
+    ]
+},
+
+{
+    enunciado: "qual o nome e como foi criado o estádio de Maringá?" ,
+    alternativas: [ "Alternativas 1",
+    "Alternativa 2"
+
+    ]
+}
 ]
